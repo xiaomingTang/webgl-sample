a92bc70c6dd2d2e85d392f2bd4f8f85db64a3445
CiroLee <ciromail@126.com>
2019-07-19 16:16:37 +0800

    add uniform

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -8,35 +8,48 @@ import fragmentShaderSource from "@Src/components/glsl/fragmentShader.glsl"
 const canvas = document.querySelector("#canvas") as HTMLCanvasElement
 const gl = canvas.getContext("webgl")
 
-window.addEventListener("resize", () => {
+const resizeCanvas = function resizeCanvas() {
   if (gl) {
     resizeToDisplaySize(canvas)
     const { width, height } = canvas
     gl.viewport(0, 0, width, height)
   }
-})
+}
+
+resizeCanvas()
+
+window.addEventListener("resize", resizeCanvas)
 
 if (gl) {
   const vertexShader = createGLShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
   const fragmentShader = createGLShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
   const program = createGLProgram(gl, vertexShader, fragmentShader)
   if (program) {
-    const positions = new Float32Array([0, 0, 0, 0.5, 0.7, 0])
-    const posAttrLoc = gl.getAttribLocation(program, "a_position")
-    const posBuffer = gl.createBuffer() as WebGLBuffer
-    gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW)
-
-    gl.clearColor(0, 0, 0, 0)
+    gl.clearColor(0, 0, 0, 1)
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.useProgram(program)
+
+    const positions = new Float32Array([
+      0, 0,
+      0, 180,
+      360, 360,
+      18, 0,
+      360, 180,
+      180, 0,
+    ])
+    const posAttrLoc = gl.getAttribLocation(program, "a_position")
+    const posBuffer = gl.createBuffer() as WebGLBuffer
     gl.enableVertexAttribArray(posAttrLoc)
     gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW)
 
     const [size, type, normalize, stride, offset1] = [2, gl.FLOAT, false, 0, 0]
     gl.vertexAttribPointer(posAttrLoc, size, type, normalize, stride, offset1)
 
-    const [primitiveType, offset2, count] = [gl.TRIANGLES, 0, 3]
+    const resolutionUnifLoc = gl.getUniformLocation(program, "u_resolution")
+    gl.uniform2f(resolutionUnifLoc, gl.canvas.width, gl.canvas.height)
+
+    const [primitiveType, offset2, count] = [gl.TRIANGLES, 0, 6]
     gl.drawArrays(primitiveType, offset2, count)
   }
 }
--- src/components/glsl/vertexShader.glsl
+++ src/components/glsl/vertexShader.glsl
@@ -1,5 +1,11 @@
-attribute vec4 a_position;
+attribute vec2 a_position;
+uniform vec2 u_resolution;
 
 void main() {
-  gl_Position = a_position;
+  gl_Position = vec4(
+    a_position.x / u_resolution.x * 2.0 - 1.0,
+    -(a_position.y / u_resolution.y * 2.0 - 1.0),
+    1.0,
+    1.0
+  );
 }
